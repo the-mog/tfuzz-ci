adce951e469a29054f66f48b38fdce77adf93645
themog <themog@tari.com>
2020-10-16 10:38:28 +0200

    update with the build flag

--- ci/fuzzit.sh
+++ ci/fuzzit.sh
@@ -3,11 +3,12 @@ set -xe
 ## build fuzzer
 #cargo fuzz run mmr_push_bytes -- -runs=0
 
-wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.72/fuzzit_Linux_x86_64
-chmod a+x fuzzit
+mkdir fuzztari
+cd fuzztari
+git clone https://github.com/tari-project/tari
+git clone https://github.com/tari-project/tari_utilities
+git clone https://github.com/tari-project/tari-crypto
+git clone https://github.com/the-mog/tari-fuzzer
+cd tari-fuzzer
+cargo run build
 
-if [ $1 == "fuzzing" ]; then
-    ./fuzzit create job mmr_push_bytes ./targets/mmr_push_bytes
-else
-    ./fuzzit create job --type local-regression mmr_push_bytes ./targets/mmr_push_bytes --additional-corpus ./corpus-mmr_push_bytes.tar.gz
-fi
